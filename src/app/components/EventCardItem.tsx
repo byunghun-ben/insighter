cd3a3890d9b3e62d3024409b371c88e641bf8371
ByunghunKim <byunghun@publy.co>
2024-06-19 12:06:36 +0900

    불필요 컴포넌트 정리

--- src/app/components/EventCardItem.tsx
+++ src/app/components/EventCardItem.tsx
@@ -1,8 +1,8 @@
-import { Description, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import { Link } from "react-router-dom";
 import { Event } from "../../types";
 import useEventContext from "../hooks/useEventContext";
+import EventDeleteModal from "./EventDeleteModal";
 
 type Props = {
   event: Event;
@@ -23,47 +23,13 @@ const EventCardItem = ({ event }: Props) => {
         <span className="text-sm text-slate-500">{event.time}</span>
       </div>
       <span className="text-slate-700">{event.description}</span>
-      <Link to={`/edit/${event.id}`} className="text-blue-500 underline">
-        수정
-      </Link>
-      <DeleteModal onConfirm={deleteEvent} />
+      <div className="self-end flex items-center gap-2">
+        <Link to={`/edit/${event.id}`} className="text-sm text-blue-500">
+          수정
+        </Link>
+        <EventDeleteModal onConfirm={deleteEvent} />
+      </div>
     </li>
-  );
-};
-
-type DeleteModalProps = {
-  onConfirm: () => void;
-};
-
-const DeleteModal = ({ onConfirm }: DeleteModalProps) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
-  return (
-    <>
-      <button onClick={openModal}>삭제</button>
-      <Dialog open={isModalOpen} as="div" className="relative z-10 focus:outline-none" onClose={closeModal}>
-        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="w-full max-w-sm space-y-4 bg-white p-6 rounded-lg">
-            <DialogTitle className="font-bold text-xl">이벤트 삭제</DialogTitle>
-            <Description>이 이벤트를 삭제하시겠어요?</Description>
-            <div className="flex justify-end gap-4">
-              <button
-                onClick={onConfirm}
-                className="flex py-2 px-4 bg-slate-700 text-white rounded-lg hover:bg-slate-800"
-              >
-                삭제
-              </button>
-              <button onClick={closeModal} className="flex py-2 px-4 bg-white rounded-lg hover:bg-slate-100">
-                취소
-              </button>
-            </div>
-          </DialogPanel>
-        </div>
-      </Dialog>
-    </>
   );
 };
 
